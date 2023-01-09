4a8d634906153fb65791beb948ee6a33d2550094
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2023-01-09 21:28:08 +0530

    firsttest

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,17 +6,17 @@ function Slider() {
        <>
         <div id="carouselExampleIndicators" className="carousel slide">
           <div className="carousel-inner container">
-            <div className="carousel-item active">
+            <div className="carousel-item container active">
             <div className="wrapper">  
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/XCjbU13UaBk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item container">
             <div className='wrapper'>
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/g7Ih8j0jwqE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item container">
             <div className='wrapper'>  
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/h-0mncOcGcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
