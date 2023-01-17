b88b44a369f32036df4acbfa49606440afd16a23
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2023-01-18 04:00:29 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
             <div className="container-fluid">
                 <div style={{display:"flex"}}><Link className="navbar-brand d-flex" to="/"><h1 className='text-primary d-flex'>Qy</h1>_AMV</Link></div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
--- src/components/SlideItem.js
+++ src/components/SlideItem.js
@@ -6,10 +6,11 @@ export default class SlideItem extends Component {
    
     return (
         <div className={`carousel-item ${ac}`}>
-            <div className='d-flex' style={{height:"66.7px"}}><h1 className='text-primary'>{id}</h1><h1 className='text-light'>.{title}</h1></div>
             <div className="wrapper">  
                  <iframe className="d-block w-100 " src={vSrc} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
+            <div className='d-flex' style={{height:"66.7px"}}><h1 className='text-primary'>{id}</h1><h1 className='text-light'>.{title}</h1></div>
+
         </div>
     )
   }
