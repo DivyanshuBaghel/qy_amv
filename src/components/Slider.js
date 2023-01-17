3e80e20c9c6ea48d9140e0d0dadacb47067adab4
Divyanshu Baghel <85153304+DivyanshuBaghel@users.noreply.github.com>
2023-01-17 23:09:04 +0530

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,17 +8,17 @@ function Slider() {
           <div className="carousel-inner">
             <div className="carousel-item active">
             <div className="wrapper">  
-              <iframe className="d-block w-100 " src="https://www.youtube.com/embed/XCjbU13UaBk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
+              <iframe className="d-block w-95 " src="https://www.youtube.com/embed/XCjbU13UaBk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
             </div>
             <div className="carousel-item">
             <div className='wrapper'>
-              <iframe className="d-block w-100 " src="https://www.youtube.com/embed/g7Ih8j0jwqE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+              <iframe className="d-block w-95 " src="https://www.youtube.com/embed/g7Ih8j0jwqE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             </div>
             <div className="carousel-item">
             <div className='wrapper'>  
-              <iframe className="d-block w-100 " src="https://www.youtube.com/embed/h-0mncOcGcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
+              <iframe className="d-block w-95 " src="https://www.youtube.com/embed/h-0mncOcGcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
             </div>
           </div>
