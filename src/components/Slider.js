11f5d74467c5197e542cdf32bb9faa2385010c99
Divyanshu Baghel <85153304+DivyanshuBaghel@users.noreply.github.com>
2023-01-17 23:02:46 +0530

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -11,12 +11,12 @@ function Slider() {
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/XCjbU13UaBk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
             </div>
-            <div className="carousel-item container">
+            <div className="carousel-item">
             <div className='wrapper'>
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/g7Ih8j0jwqE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             </div>
-            <div className="carousel-item container">
+            <div className="carousel-item">
             <div className='wrapper'>  
               <iframe className="d-block w-100 " src="https://www.youtube.com/embed/h-0mncOcGcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
             </div>
